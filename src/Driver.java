1c7aed685b46a6a9ac2e4872c8cb701a7d58ad61
Aditya V. Shirode <avshirod@ncsu.edu>
2017-04-10 14:35:09 -0400

    Changing text placement wrt resolution

--- src/Driver.java
+++ src/Driver.java
@@ -19,7 +19,8 @@ public class Driver extends PApplet
 
 	public void settings()
 	{
-		size(1200, 700);
+		size(5760/2, 2304/2);
+		//size(2880, 2440);
 	}
 
 	public void setup() 
@@ -37,14 +38,15 @@ public class Driver extends PApplet
 	void printTime()
 	{
 		//textMode(SHAPE);
-		int fontSize = width * height / 18600;
+		//int fontSize = width * height / 51840;
+		int fontSize = 128;
 		
 		PFont myFont = createFont("JosefinSans-Regular.ttf", fontSize);
 		textFont(myFont);
 		textSize(fontSize);
 
 		// Source: http://stackoverflow.com/questions/4216745/java-string-to-date-conversion/
-		DateFormat dateFormat = new SimpleDateFormat("E MMMM dd, yyyy    hh:mm:ss a", Locale.ENGLISH);
+		DateFormat dateFormat = new SimpleDateFormat("E MMMM dd, yyyy     hh:mm:ss a", Locale.ENGLISH);
 		DateTimeFormatter dateformat = DateTimeFormatter.ofPattern("E  MMMM dd, yyyy");
 		DateTimeFormatter timeformat = DateTimeFormatter.ofPattern("hh:mm:ss a");
 
@@ -67,9 +69,9 @@ public class Driver extends PApplet
 
 		// The multiplication factor is decided based on Max Possible TextWidth for a given fontSize
 		// As we can't find maximum dynamically, we've scaled them based on observed values at fontSize 64 
-		float maxTextWid = 1000 * fontSize / 64;
-		float maxDateWid = 635 * fontSize / 64;
-		float dateTimeWid = 900 * fontSize / 64;
+		float maxTextWid = 1050 * fontSize / 64;
+		float maxDateWid = 685 * fontSize / 64;
+		float dateTimeWid = 935 * fontSize / 64;
 
 		//textAlign(CENTER, CENTER);
 		//text(d, pos.x - textWid/2, pos.y + textDescent());
@@ -77,7 +79,21 @@ public class Driver extends PApplet
 		text(displayTime, pos.x - maxTextWid/2 + maxDateWid,  pos.y + textDescent());
 		text(timeMarker,  pos.x - maxTextWid/2 + dateTimeWid, pos.y + textDescent());
 		
-		//text("#GoWolfPack", , );
+		fontSize /= 1.5;
+		textSize(fontSize);
+		fill(204,0,0);
+		
+		myFont = createFont("JosefinSans-BoldItalic.ttf", fontSize);
+		textFont(myFont);
+		String displayMessage1 = "#BecomeTheAlpha";
+		text(displayMessage1, pos.x - maxTextWid/2 + textWidth(displayDate)/2.5f,
+				pos.y + fontSize*1.5f + textDescent());
+		
+		myFont = createFont("JosefinSans-Italic.ttf", fontSize);
+		textFont(myFont);
+		String displayMessage2 = "#GoWolfPack";
+		text(displayMessage2, pos.x + maxDateWid/2 - textWidth(displayMessage2)/1.8f,
+				pos.y + fontSize*1.5f + textDescent());
 
 //		textMode(MODEL);
 	}
